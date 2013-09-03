55b0eb1f79d635267ab1d5c2805276b06bec6641
Eric Panorel <ericpanorel@users.noreply.github.com>
2013-09-03 11:23:51 -0600

    moved saving height

--- indiClick.js
+++ indiClick.js
@@ -9,19 +9,19 @@
         return directive;
         function link(scope, element, attr) {
             var fn = $parse(attr[directiveId]),
-            target = element[0],
-            height = element.height(),
-            oldWidth = element.width(),
-            opts = {
-                length: Math.round(height / 3),
-                radius: Math.round(height / 5),
-                width: Math.round(height / 10),
-                color: element.css("color"),
-                left: -5
-            }; // customize this "resizing and coloring" algorithm
+            target = element[0];
 
             element.on('click', function (event) {
                 scope.$apply(function () {
+                    var height = element.height(),
+                    oldWidth = element.width(),
+                    opts = {
+                        length: Math.round(height / 3),
+                        radius: Math.round(height / 5),
+                        width: Math.round(height / 10),
+                        color: element.css("color"),
+                        left: -5
+                    }; // customize this "resizing and coloring" algorithm
                     attr.$set('disabled', true);
                     element.width(oldWidth + oldWidth / 2); // make room for spinner
 
